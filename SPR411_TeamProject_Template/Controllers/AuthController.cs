d06b899a6c2c9627b765644e862c9f2cd1fcd86f
potapchukdmytro <traiget@gmail.com>
2026-05-02 11:09:07 +0300

    Add login method

--- SPR411_TeamProject_Template/Controllers/AuthController.cs
+++ SPR411_TeamProject_Template/Controllers/AuthController.cs
@@ -6,9 +6,16 @@ namespace SPR411_TeamProject_Template.Controllers
     [Route("/api/auth")]
     public class AuthController : ControllerBase
     {
+        [HttpGet]
         public IActionResult Get()
         {
             return Ok("Hello World");
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return Ok("Login");
         }
     }
 }
