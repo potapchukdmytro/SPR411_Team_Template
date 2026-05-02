a4483a846dfd061d460832baf2b3be18b6a83db8
potapchukdmytro <traiget@gmail.com>
2026-05-02 11:50:48 +0300

    Login dto

---
+++ SPR411_TeamProject_Template/Dtos/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SPR411_TeamProject_Template.Dtos
+{
+    public class LoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
