49054f07830bc9dd78757de1c4737354d045b4bc
potapchukdmytro <traiget@gmail.com>
2026-05-02 11:07:43 +0300

    Remove default controller. Created Auth controller

---
+++ SPR411_TeamProject_Template/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPR411_TeamProject_Template.Controllers
+{
+    [ApiController]
+    [Route("/api/auth")]
+    public class AuthController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
